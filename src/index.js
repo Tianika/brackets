26db87b5f2bb6e85a2be761eb4c7802a2eca0742
Tianika <tatyriana@yandex.ru>
2021-08-25 07:46:28 +0300

    half task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let temp = []
+  let brackets = {}
+
+  for (let k = 0; k < bracketsConfig.length; k++) {
+    brackets[bracketsConfig[k][0]] = bracketsConfig[k][1]
+  }
+
+  console.log(brackets)
+
+  for (let i = 0; i < str.length; i++) {
+    if (Object.keys(brackets).includes(str[i])) {
+      temp.push(str[i])
+    } else if (Object.values(brackets).includes(str[i])) {
+      if (temp.length > 0) {
+        if (brackets[temp[temp.length - 1]] === str[i]) {
+          temp.pop()
+        } else {
+          return false
+        }
+      }
+    }
+  }
+
+  if (temp.length === 0) {
+    return true
+  } else {
+    return false
+  }
 }
